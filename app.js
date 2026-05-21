a19ac497c3469077d9b2ac94d520e8a3e2dd0940
RGPin <157803400+RGPin@users.noreply.github.com>
2026-05-21 10:48:58 +0800

    Set up ejs and router

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require("express");
+const path = require("node:path");
 
 const app = express();
+const indexRouter = require("./routers/indexRouter");
 
-app.get("/", (req, res) => res.send("Hello world"));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use("/", indexRouter);
 
 const PORT = 8000;
 app.listen(PORT, (error) => {
